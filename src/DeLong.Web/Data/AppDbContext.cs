cba44fc932bb8a58ad8767707eb46a880f984b57
trankhacvinh <trankhacvinh@users.noreply.github.com>
2026-08-10 22:17:25 +0700

    feat: map customer and booking entities

--- src/DeLong.Web/Data/AppDbContext.cs
+++ src/DeLong.Web/Data/AppDbContext.cs
@@ -17,6 +17,8 @@ public sealed class AppDbContext
     public DbSet<Property> Properties => Set<Property>();
     public DbSet<Room> Rooms => Set<Room>();
     public DbSet<RoomRate> RoomRates => Set<RoomRate>();
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Booking> Bookings => Set<Booking>();
     public DbSet<UserPropertyAccess> UserPropertyAccesses => Set<UserPropertyAccess>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -50,6 +52,48 @@ public sealed class AppDbContext
                 .WithMany(x => x.Rates)
                 .HasForeignKey(x => x.RoomId)
                 .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasIndex(x => new { x.PropertyId, x.NormalizedPhone }).IsUnique();
+            entity.HasIndex(x => new { x.PropertyId, x.Name });
+            entity.Property(x => x.Name).HasMaxLength(200).IsRequired();
+            entity.Property(x => x.Phone).HasMaxLength(30).IsRequired();
+            entity.Property(x => x.NormalizedPhone).HasMaxLength(30).IsRequired();
+            entity.Property(x => x.Email).HasMaxLength(254);
+            entity.Property(x => x.IdentityNumber).HasMaxLength(100);
+            entity.Property(x => x.Note).HasMaxLength(2000);
+            entity.HasOne(x => x.Property)
+                .WithMany()
+                .HasForeignKey(x => x.PropertyId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<Booking>(entity =>
+        {
+            entity.HasIndex(x => new { x.PropertyId, x.Code }).IsUnique();
+            entity.HasIndex(x => new { x.PropertyId, x.Status });
+            entity.HasIndex(x => new { x.RoomId, x.CheckInUtc, x.CheckOutUtc });
+            entity.Property(x => x.Code).HasMaxLength(30).IsRequired();
+            entity.Property(x => x.Status).HasConversion<string>().HasMaxLength(30).IsRequired();
+            entity.Property(x => x.RoomAmount).HasPrecision(18, 2);
+            entity.Property(x => x.ExtraAmount).HasPrecision(18, 2);
+            entity.Property(x => x.DiscountAmount).HasPrecision(18, 2);
+            entity.Property(x => x.Source).HasMaxLength(100);
+            entity.Property(x => x.Note).HasMaxLength(2000);
+            entity.HasOne(x => x.Property)
+                .WithMany()
+                .HasForeignKey(x => x.PropertyId)
+                .OnDelete(DeleteBehavior.Restrict);
+            entity.HasOne(x => x.Room)
+                .WithMany()
+                .HasForeignKey(x => x.RoomId)
+                .OnDelete(DeleteBehavior.Restrict);
+            entity.HasOne(x => x.Customer)
+                .WithMany()
+                .HasForeignKey(x => x.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
         });
 
         modelBuilder.Entity<UserPropertyAccess>(entity =>
