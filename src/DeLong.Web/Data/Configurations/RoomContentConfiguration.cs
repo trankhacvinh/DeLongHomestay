65e83afc126adedf912c09c96e89666396b8e560
trankhacvinh <trankhacvinh@users.noreply.github.com>
2026-08-14 11:49:35 +0700

    feat: configure focal points and amenity presets

--- src/DeLong.Web/Data/Configurations/RoomContentConfiguration.cs
+++ src/DeLong.Web/Data/Configurations/RoomContentConfiguration.cs
@@ -27,6 +27,8 @@ public sealed class RoomImageConfiguration : IEntityTypeConfiguration<RoomImage>
         entity.Property(x => x.ThumbnailPath).HasMaxLength(500).IsRequired();
         entity.Property(x => x.ContentType).HasMaxLength(100).IsRequired();
         entity.Property(x => x.AltText).HasMaxLength(300);
+        entity.Property(x => x.FocalX).HasDefaultValue(0.5d);
+        entity.Property(x => x.FocalY).HasDefaultValue(0.5d);
         entity.HasOne(x => x.Room).WithMany(x => x.Images).HasForeignKey(x => x.RoomId).OnDelete(DeleteBehavior.Cascade);
     }
 }
@@ -50,6 +52,27 @@ public sealed class RoomAmenityConfiguration : IEntityTypeConfiguration<RoomAmen
         entity.HasKey(x => new { x.RoomId, x.AmenityId });
         entity.HasOne(x => x.Room).WithMany(x => x.Amenities).HasForeignKey(x => x.RoomId).OnDelete(DeleteBehavior.Cascade);
         entity.HasOne(x => x.Amenity).WithMany(x => x.Rooms).HasForeignKey(x => x.AmenityId).OnDelete(DeleteBehavior.Cascade);
+    }
+}
+
+public sealed class AmenityPresetConfiguration : IEntityTypeConfiguration<AmenityPreset>
+{
+    public void Configure(EntityTypeBuilder<AmenityPreset> entity)
+    {
+        entity.HasIndex(x => new { x.PropertyId, x.NormalizedName }).IsUnique();
+        entity.Property(x => x.Name).HasMaxLength(120).IsRequired();
+        entity.Property(x => x.NormalizedName).HasMaxLength(120).IsRequired();
+        entity.HasOne(x => x.Property).WithMany().HasForeignKey(x => x.PropertyId).OnDelete(DeleteBehavior.Cascade);
+    }
+}
+
+public sealed class AmenityPresetItemConfiguration : IEntityTypeConfiguration<AmenityPresetItem>
+{
+    public void Configure(EntityTypeBuilder<AmenityPresetItem> entity)
+    {
+        entity.HasKey(x => new { x.AmenityPresetId, x.AmenityId });
+        entity.HasOne(x => x.AmenityPreset).WithMany(x => x.Items).HasForeignKey(x => x.AmenityPresetId).OnDelete(DeleteBehavior.Cascade);
+        entity.HasOne(x => x.Amenity).WithMany().HasForeignKey(x => x.AmenityId).OnDelete(DeleteBehavior.Cascade);
     }
 }
 
