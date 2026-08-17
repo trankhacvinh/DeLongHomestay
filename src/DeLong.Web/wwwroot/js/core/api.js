560361dee7716cac52ebb7320dd2e49874fa4eb3
trankhacvinh <trankhacvinh@users.noreply.github.com>
2026-08-17 13:50:22 +0700

    chore: bump flexible menu assets

--- src/DeLong.Web/wwwroot/js/core/api.js
+++ src/DeLong.Web/wwwroot/js/core/api.js
@@ -70,10 +70,10 @@
         addStyle('data-public-row-templates', '/css/public-row-templates.css?v=20260817-1');
         addStyle('data-public-editor-enhancements', '/css/public-visual-editor-enhancements.css?v=20260817-2');
         addStyle('data-public-contextual-editor', '/css/public-contextual-editor.css?v=20260817-1');
-        addStyle('data-public-contextual-shell-rates', '/css/public-contextual-shell-rates.css?v=20260817-1');
+        addStyle('data-public-contextual-shell-rates', '/css/public-contextual-shell-rates.css?v=20260817-2');
         addStyle('data-public-contextual-content-cards', '/css/public-contextual-content-cards.css?v=20260817-1');
 
-        addScript('data-public-shell-runtime', '/js/pages/public-shell-runtime.js?v=20260817-1');
+        addScript('data-public-shell-runtime', '/js/pages/public-shell-runtime.js?v=20260817-2');
         addScript('data-public-rich-editor', '/js/pages/public-rich-editor.js?v=20260817-3');
         addScript('data-public-editorial-order', '/js/pages/public-editorial-order.js?v=20260817-2');
         addScript('data-public-visual-editor', '/js/pages/public-visual-editor-v2.js?v=20260817-4');
@@ -81,7 +81,7 @@
         addScript('data-public-row-editor', '/js/pages/public-visual-row-builder-v4.js?v=20260817-1');
         addScript('data-public-row-templates', '/js/pages/public-visual-row-templates.js?v=20260817-1');
         addScript('data-public-contextual-editor', '/js/pages/public-contextual-editor.js?v=20260817-1');
-        addScript('data-public-contextual-shell-rates', '/js/pages/public-contextual-shell-rates.js?v=20260817-1');
+        addScript('data-public-contextual-shell-rates', '/js/pages/public-contextual-shell-rates.js?v=20260817-2');
         addScript('data-public-contextual-content-cards', '/js/pages/public-contextual-content-cards.js?v=20260817-1');
     }
 })(window);
