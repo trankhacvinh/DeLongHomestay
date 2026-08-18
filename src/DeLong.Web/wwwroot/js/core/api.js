b5eb820e6c36fcceb0b92594e5db1099a490afb7
trankhacvinh <trankhacvinh@users.noreply.github.com>
2026-08-18 11:13:12 +0700

    feat: load media library on visual editor

--- src/DeLong.Web/wwwroot/js/core/api.js
+++ src/DeLong.Web/wwwroot/js/core/api.js
@@ -79,6 +79,7 @@
         addStyle('data-public-row-inline', '/css/public-row-inline-editor.css?v=20260818-2');
         addStyle('data-public-row-basic-elements-fix', '/css/public-row-basic-elements-fix.css?v=20260818-1');
         addStyle('data-public-row-advanced-elements', '/css/public-row-advanced-elements.css?v=20260818-1');
+        addStyle('data-public-media-library', '/css/public-media-library.css?v=20260818-1');
 
         addScript('data-public-shell-runtime', '/js/pages/public-shell-runtime.js?v=20260817-2');
         addScript('data-public-rich-editor', '/js/pages/public-rich-editor.js?v=20260817-3');
@@ -100,6 +101,7 @@
         addScript('data-public-row-inline-bindings', '/js/pages/public-row-inline-bindings.js?v=20260818-1');
         addScript('data-public-row-advanced-elements', '/js/pages/public-row-advanced-elements.js?v=20260818-1');
         addScript('data-public-row-advanced-runtime', '/js/pages/public-row-advanced-runtime.js?v=20260818-1');
+        addScript('data-public-media-library', '/js/pages/public-media-library.js?v=20260818-1');
         addScript('data-public-inline-editor', '/js/pages/public-inline-editor.js?v=20260817-1');
     }
 })(window);
