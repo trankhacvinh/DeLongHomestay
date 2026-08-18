9c0d233a035127b0bd9ba51898baf48650c2c456
trankhacvinh <trankhacvinh@users.noreply.github.com>
2026-08-18 13:33:47 +0700

    feat: load footer inline editor and toggle polish

--- src/DeLong.Web/wwwroot/js/core/api.js
+++ src/DeLong.Web/wwwroot/js/core/api.js
@@ -99,6 +99,7 @@
         addStyle('data-public-media-library', '/css/public-media-library.css?v=20260818-1');
         addStyle('data-public-toolbar-compact', '/css/public-toolbar-compact.css?v=20260818-1');
         addStyle('data-public-header-footer-designer', '/css/public-header-footer-designer.css?v=20260818-1');
+        addStyle('data-public-footer-inline-editor', '/css/public-footer-inline-editor.css?v=20260818-1');
 
         addScript('data-public-shell-runtime', '/js/pages/public-shell-runtime.js?v=20260817-2');
         addScript('data-public-shell-designer-runtime', '/js/pages/public-shell-designer-runtime.js?v=20260818-1');
@@ -125,5 +126,6 @@
         addScript('data-public-inline-editor', '/js/pages/public-inline-editor.js?v=20260817-1');
         addScript('data-public-toolbar-compact', '/js/pages/public-toolbar-compact.js?v=20260818-1');
         addScript('data-public-header-footer-designer', '/js/pages/public-header-footer-designer.js?v=20260818-1');
+        addScript('data-public-footer-inline-editor', '/js/pages/public-footer-inline-editor.js?v=20260818-1');
     }
 })(window);
